a18bc0544e2f7e24ca2e2b9d71e994d692ffdc34
20171cse0558 <60248967+20171cse0558@users.noreply.github.com>
2020-01-24 10:53:53 +0530

    Add files via upload

---
+++ malloc.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+int arr[2],i;
+int ptr=arr;
+int ptr_new;
+arr[0]=10;
+arr[1]=20;
+ptr=(int)relloc(ptr,size of(int)*3);
+(ptr_new + 2) = 30;
+printf("%d",(ptr_new+i));
+getchar();
+return 0;
+}
+
